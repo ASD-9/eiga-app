a8e977a8328db8b645eac676c67411a40de8c382
ASD-9 <ryan.chavatte@gmail.com>
2025-05-05 17:09:57 +0200

    dio client configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:dio/dio.dart';
 import 'package:eiga/routes/app_router.dart';
 import 'package:eiga/themes/app_theme.dart';
+import 'package:eiga/utils/dio_client.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:go_router/go_router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load();
+
+  final Dio dio = DioClient.dio;
 
   GoRouter router = await getAppRouter();
   ThemeData theme = await getAppTheme();
